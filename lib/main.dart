2ff4825e6a0b52008098aae4c09d40fbe623e206
JosephGakah <joegakah@gmail.com>
2025-02-17 23:16:40 +0300

    Initialize Firebase and Hive; register chat model adapters for data handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:buai/app.dart';
 import 'package:buai/firebase_options.dart';
+import 'package:buai/models/chat_content_model.dart';
+import 'package:buai/models/chat_model.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(ChatModelAdapter());
+  Hive.registerAdapter(ChatContentModelAdapter());
+
   runApp(const App());
 }
