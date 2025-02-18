d1a10c08521e6f2468221b0dac366461b5d455c7
JosephGakah <joegakah@gmail.com>
2025-02-18 14:29:16 +0300

    Enhance ChatBubble UI with user display name and response translation; add action buttons for copy, audio, and share

--- lib/ui/pages/chat/chat_bubble.dart
+++ lib/ui/pages/chat/chat_bubble.dart
@@ -1,23 +1,124 @@
 import 'package:buai/models/chat_content_model.dart';
+import 'package:buai/ui/widgets/app_buttons.dart';
 import 'package:buai/utils/colors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatBubble extends StatelessWidget {
-  const ChatBubble({super.key, required this.chat});
+  ChatBubble({super.key, required this.chat});
   final ChatContentModel chat;
+  final User user = FirebaseAuth.instance.currentUser!;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: AppColors.secondaryWhite,
-        borderRadius: BorderRadius.circular(5),
-        border: Border.all(width: 1.5, color: Colors.black),
-      ),
-      child: Column(
-        children: [],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Container(
+              height: 20,
+              width: 20,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/avatar.png'),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(60),
+                border: Border.all(
+                  width: 2,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Text(
+                user.displayName ?? '',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Text(
+          chat.chat ?? '',
+          style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 10),
+        Container(
+          color: AppColors.primaryGrey.withAlpha((0.15 * 255).ceil()),
+          padding: EdgeInsets.all(3),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    height: 20,
+                    width: 20,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/avatar.png'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(60),
+                      border: Border.all(
+                        width: 2,
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Text(
+                      'junubi ai',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 5),
+              Text(
+                chat.translatedResponse ?? '',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            AppButtons.circularButton(
+              onPressed: () {},
+              context: context,
+              color: AppColors.primaryBlue,
+              icon: Icons.copy_sharp,
+            ),
+            const SizedBox(width: 5),
+            AppButtons.circularButton(
+              onPressed: () {},
+              context: context,
+              color: AppColors.primaryBlue,
+              icon: Icons.volume_up,
+            ),
+            const SizedBox(width: 5),
+            AppButtons.circularButton(
+              onPressed: () {},
+              context: context,
+              color: AppColors.primaryBlue,
+              icon: Icons.ios_share,
+            ),
+          ],
+        )
+      ],
     );
   }
 }
