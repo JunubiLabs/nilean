32202232b752de4b02146d0e4601c522b242f48a
JosephGakah <joegakah@gmail.com>
2025-03-04 21:05:29 +0300

    Refactor RecommendedNews to fetch news dynamically from NewsRepository, replacing static data with FutureBuilder for improved data handling and loading state management

--- lib/ui/pages/home/recommended_news.dart
+++ lib/ui/pages/home/recommended_news.dart
@@ -1,5 +1,7 @@
+import 'package:buai/repositories/news_repository.dart';
 import 'package:buai/ui/widgets/app_cards.dart';
 import 'package:buai/ui/widgets/app_texts.dart';
+import 'package:card_loading/card_loading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
@@ -12,25 +14,6 @@ class RecommendedNews extends StatefulWidget {
 }
 
 class _RecommendedNewsState extends State<RecommendedNews> {
-  List news = [
-    [
-      'Kä ji̱ South Sudan ti ŋuan ti ci li̱w rɛy köör Sudan in rɛwdɛ?',
-      'https://i0.wp.com/www.nyamile.com/wp-content/uploads/2024/10/nasirprotests.jpg?w=960&ssl=1'
-    ],
-    [
-      'Yeŋö ye ɣɛn wïc ba dɔ̈ɔ̈r bɛ̈i në piny ciɛɛm de Sudan yic në thaa thiɔ̈kic?',
-      'https://i0.wp.com/www.nyamile.com/wp-content/uploads/2021/05/South-Sudan-Humanitarian-Catastrophe.jpg?w=1024&ssl=1'
-    ],
-    [
-      'Yeŋö ye ɣɛn wïc ba dɔ̈ɔ̈r bɛ̈i në piny ciɛɛm de Sudan yic në thaa thiɔ̈kic?',
-      'https://i0.wp.com/www.nyamile.com/wp-content/uploads/2021/05/South-Sudan-Humanitarian-Catastrophe.jpg?w=1024&ssl=1'
-    ],
-    [
-      'Yeŋö ye ɣɛn wïc ba dɔ̈ɔ̈r bɛ̈i në piny ciɛɛm de Sudan yic në thaa thiɔ̈kic?',
-      'https://i0.wp.com/www.nyamile.com/wp-content/uploads/2021/05/South-Sudan-Humanitarian-Catastrophe.jpg?w=1024&ssl=1'
-    ],
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -43,29 +26,45 @@ class _RecommendedNewsState extends State<RecommendedNews> {
           context: context,
         ),
         const SizedBox(height: 20),
-        StaggeredGrid.count(
-          crossAxisCount: 2,
-          mainAxisSpacing: 15,
-          crossAxisSpacing: 10,
-          children: [
-            for (var n in news) ...[
-              AppCards.curatedNewsCard(
-                context: context,
-                news: n[0],
-                image: n[1],
-                onPressed: () {},
-              )
-                  .animate()
-                  .slideY(
-                    curve: Curves.ease,
-                    duration: const Duration(milliseconds: 700),
-                    delay: Duration(milliseconds: news.indexOf(n) * 100),
-                    begin: 0.5,
-                  )
-                  .fade(),
-            ]
-          ],
-        ),
+        FutureBuilder(
+          future: NewsRepository().fetchNews(),
+          builder: (context, snapshot) {
+            return StaggeredGrid.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 15,
+              crossAxisSpacing: 10,
+              children: [
+                if (snapshot.connectionState == ConnectionState.waiting) ...[
+                  for (var i = 0; i < 3; i++) ...[
+                    CardLoading(
+                      height: 120,
+                    ),
+                  ],
+                ],
+                if (snapshot.hasData) ...[
+                  for (var news in snapshot.data!.sublist(0, 4)) ...[
+                    AppCards.curatedNewsCard(
+                      context: context,
+                      news: news.title,
+                      image: news.imageUrl,
+                      onPressed: () {},
+                    )
+                        .animate()
+                        .slideY(
+                          curve: Curves.ease,
+                          duration: const Duration(milliseconds: 700),
+                          delay: Duration(
+                              milliseconds:
+                                  (snapshot.data?.indexOf(news) ?? 0) * 100),
+                          begin: 0.5,
+                        )
+                        .fade(),
+                  ],
+                ]
+              ],
+            );
+          },
+        )
       ],
     );
   }
