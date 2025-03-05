0c662b8aa04a053ed788ef41889ad293f2e5c557
JosephGakah <joegakah@gmail.com>
2025-03-05 13:50:58 +0300

    Add Firebase Messaging support and notification permissions

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:buai/gemini_options.dart';
 import 'package:buai/models/chat_content_model.dart';
 import 'package:buai/models/chat_model.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -11,6 +12,18 @@ import 'package:hive_flutter/hive_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseMessaging.instance.setAutoInitEnabled(true);
+
+  FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  FirebaseMessaging.onMessage.listen((message) {
+    print(' FCM on message  : ${message.notification?.title}');
+  });
+
   Gemini.init(apiKey: GeminiOptions.googleApiKey);
 
   await Hive.initFlutter();
