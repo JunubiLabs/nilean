09b8fbe7accd81341b6ddf5250568d3a708bacc2
JosephGakah <joegakah@gmail.com>
2025-03-17 10:53:49 +0300

    Chec auth to access account page

--- lib/ui/pages/settings/account_page.dart
+++ lib/ui/pages/settings/account_page.dart
@@ -43,131 +43,152 @@ class _AccountPageState extends State<AccountPage> {
                 Navigator.pop(context);
               }),
               const SizedBox(height: 20),
-              Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: _profilePictureUrl != null
-                              ? NetworkImage(_profilePictureUrl ?? '')
-                              : AssetImage('assets/images/avatar.png'),
-                          fit: BoxFit.cover,
-                        ),
-                        borderRadius: BorderRadius.circular(60),
-                        border: Border.all(
-                          width: 2,
-                          color: Theme.of(context).colorScheme.secondary,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Row(
-                      children: [
-                        AppButtons.ellipsisButton(
-                          onPressed: _pickProfilePicture,
-                          text: 'Update Profile Picture',
-                          icon: Icons.arrow_outward,
-                          color: AppColors.primaryOrange,
-                          context: context,
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 20),
-
-                    // Name
-                    TextFormField(
-                      controller: _nameController,
-                      decoration: InputDecoration(
-                        labelText: 'Name',
-                        border: OutlineInputBorder(),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter your name';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(height: 10),
-
-                    // Password
-                    TextFormField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      style: GoogleFonts.lato(fontSize: 15),
-                      decoration: InputDecoration(
-                        labelText: 'New Password',
-                        border: OutlineInputBorder(),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.length < 8) {
-                          return 'Password must be at least 8 characters';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(height: 10),
-
-                    // Confirm Password
-                    TextFormField(
-                      controller: _confirmPasswordController,
-                      obscureText: true,
-                      style: GoogleFonts.lato(fontSize: 15),
-                      decoration: InputDecoration(
-                        labelText: 'Confirm Password',
-                        border: OutlineInputBorder(),
-                      ),
-                      validator: (value) {
-                        if (value != _passwordController.text) {
-                          return 'Passwords do not match';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(height: 20),
-
-                    // Update Button
-                    AppButtons.defButton(
-                      color: Colors.green,
-                      onPressed: _updateAccount,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Update Account',
-                            style: GoogleFonts.lato(fontSize: 15),
+              if (FirebaseAuth.instance.currentUser != null) ...[
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: _profilePictureUrl != null
+                                ? NetworkImage(_profilePictureUrl ?? '')
+                                : AssetImage('assets/images/avatar.png'),
+                            fit: BoxFit.cover,
                           ),
+                          borderRadius: BorderRadius.circular(60),
+                          border: Border.all(
+                            width: 2,
+                            color: Theme.of(context).colorScheme.secondary,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          AppButtons.ellipsisButton(
+                            onPressed: _pickProfilePicture,
+                            text: 'Update Profile Picture',
+                            icon: Icons.arrow_outward,
+                            color: AppColors.primaryOrange,
+                            context: context,
+                          )
                         ],
                       ),
-                    ),
-                    SizedBox(height: 5),
+                      SizedBox(height: 20),
 
-                    AppButtons.defButton(
-                      color: Colors.red,
-                      onPressed: _deleteAccount,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Delete Account',
-                            style: GoogleFonts.lato(fontSize: 15),
-                          ),
-                          const SizedBox(width: 5),
-                          Icon(Icons.delete_forever_rounded)
-                        ],
+                      // Name
+                      TextFormField(
+                        controller: _nameController,
+                        decoration: InputDecoration(
+                          labelText: 'Name',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your name';
+                          }
+                          return null;
+                        },
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 10),
+
+                      // Password
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        style: GoogleFonts.lato(fontSize: 15),
+                        decoration: InputDecoration(
+                          labelText: 'New Password',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.length < 8) {
+                            return 'Password must be at least 8 characters';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 10),
+
+                      // Confirm Password
+                      TextFormField(
+                        controller: _confirmPasswordController,
+                        obscureText: true,
+                        style: GoogleFonts.lato(fontSize: 15),
+                        decoration: InputDecoration(
+                          labelText: 'Confirm Password',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value != _passwordController.text) {
+                            return 'Passwords do not match';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 20),
+
+                      // Update Button
+                      AppButtons.defButton(
+                        color: Colors.green,
+                        onPressed: _updateAccount,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Update Account',
+                              style: GoogleFonts.lato(fontSize: 15),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 5),
+
+                      AppButtons.defButton(
+                        color: Colors.red,
+                        onPressed: _deleteAccount,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Delete Account',
+                              style: GoogleFonts.lato(fontSize: 15),
+                            ),
+                            const SizedBox(width: 5),
+                            Icon(Icons.delete_forever_rounded)
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+              ] else ...[
+                AppButtons.defButton(
+                  color: Colors.blueAccent,
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/signin');
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Login',
+                        style: GoogleFonts.lato(fontSize: 15),
+                      ),
+                      const SizedBox(width: 5),
+                      Icon(Icons.delete_forever_rounded)
+                    ],
+                  ),
+                ),
+              ]
             ],
           ),
         ),
